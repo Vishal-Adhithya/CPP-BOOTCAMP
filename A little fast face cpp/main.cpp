5a60dc5a19ebfa0e2131acabfa29cacc729f2649
Vishal-Adhithya <vishaladhithya19@gmail.com>
2020-06-20 20:50:19 +0530

    this program from LCO bootcamp
    LCO

---
+++ A little fast face cpp/main.cpp
@@ -0,0 +1,49 @@
+
+#include <cstdio>
+using namespace std;
+
+int main(){
+
+// puts("hi hello \n");// using \n makes a new line.
+// puts("if you have guts face me bullshit");
+// const int life = 3;                    // if you are using const you can change the variable...
+    int card;
+    card = 40;
+    int my_card = card;
+
+    int *myp;
+    myp = &my_card;
+
+
+
+    printf("your card value: %d", my_card);// we want to put %d sign in printf statement...
+    printf("you card:");
+
+
+
+
+ return 0;
+
+}
+
+
+
+// REMOVE THE  MULTIPLE LINES OF CODE ABOVE TO SEE THE CODE...
+// THE ABOVE PROGRAM DONE FOR REFERNECE ...
+
+
+
+
+
+
+
+// POINTERS NEED TO BE DECLARED IN FIRST DECLARED IN ORDER LIKE VARIABLES...
+
+
+
+
+
+
+
+
+
