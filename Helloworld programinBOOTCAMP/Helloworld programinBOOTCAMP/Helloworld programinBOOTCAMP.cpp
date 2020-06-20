78fdccc3747e98ed3115d8977830b5e8da682b82
Vishal-Adhithya <vishaladhithya19@gmail.com>
2020-06-20 20:46:22 +0530

    files from vs ide

---
+++ Helloworld programinBOOTCAMP/Helloworld programinBOOTCAMP/Helloworld programinBOOTCAMP.cpp
@@ -0,0 +1,42 @@
+// Helloworld programinBOOTCAMP.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+#include <cstdio>
+using namespace std;
+
+int main() {
+
+		puts("Click on button");
+		puts("create a new player");
+		puts("add a life to the player");
+
+		return 0;
+}
+
+
+//  THIS IS A NEW CLASS.
+
+// RETURN TYPE AND COMMENTS. THIS IS A SIMPLE CLASS.
+
+// THIS PROGRAM WILL GIVE A ERROR BECAUSE AFTER MAIN FUNCTION THERE WILL NO RUN IT WILL GIVE AN ERROR.
+
+void nothing() {
+
+	puts("this is a nothing function");
+	// void will not return anything
+}
+
+char this_is_a_beautiful_flower() {
+	return 'h';// THIS CHAR FUNCTION WILL RETURN  A CHARCTER.
+
+}
