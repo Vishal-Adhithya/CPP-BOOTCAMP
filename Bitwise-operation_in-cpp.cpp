6ab13279bdea788e15594fc901798df705cebcaf
Vishal-Adhithya <vishaladhithya19@gmail.com>
2020-06-25 13:25:53 +0530

    learned about some bitwise operation in c++ learn more about them

---
+++ Bitwise-operation_in-cpp.cpp
@@ -0,0 +1,52 @@
+// BITWISE OPERATION IN C++...
+// use this websites to calculate: https://www.convertbinary.com/decimal-to-binary/
+// use this websites to calculate: http://easyonlineconverter.com/converters/bitwise-calculator.html
+
+// 8 bits = 1 byte.
+
+#include <iostream>
+
+  int main(){
+
+    unsigned int x = 6;
+    unsigned int y = 7;
+// BITWISE VALUES ...
+  //  :
+        // &  BITWISE AND
+        // !  BITWISE OR
+        // ^  XOR
+        // ~ NOT
+        // >> LEFT SHIFT
+        // << RIGHT SHIFT
+
+
+
+    unsigned int z = x & y;
+
+    std::cout << z << std:: endl;
+
+
+
+    std::cout << z << std:: endl;
+
+    z = x ^ y;
+
+    std::cout << z << std:: endl;
+
+
+
+    std::cout << z << std:: endl;
+
+    z = x >> y;
+
+    std::cout << z << std:: endl;
+
+    z = x << y;
+
+    std::cout << z << std:: endl;
+
+
+
+
+    return 0;
+    }
