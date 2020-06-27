7cc5004a6be1e8d5a02b4e6273b308b05a9c9524
Vishal-Adhithya <vishaladhithya19@gmail.com>
2020-06-27 12:27:42 +0530

    learned about macros in c++

---
+++ lets-talk-about-macros.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#define END return 0
+#define ENDMESSAGE std::cout << "program ends her \n"
+#define LCOINT int18_t
+
+// macros are
+int main(){
+
+  int a = 4;
+  std::cout << a << std:: endl;
+
+  ENDMESSAGE;
+  END;
+
+  return 0;
+  }
